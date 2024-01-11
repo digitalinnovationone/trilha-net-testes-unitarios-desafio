65f780e91811b483fa4f7fa4fc7a7686270e53f5
Júlia Veríssimo <126585562+xjuveri@users.noreply.github.com>
2024-01-11 20:31:45 -0300

    corrigindo probleminha no commit

--- ValidacoesStringTests.cs
+++ ValidacoesStringTests.cs
@@ -52,7 +52,6 @@ public class ValidacoesStringTests
     [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
-        //TODO: Corrigir a variável "textoProcurado" seção Arrange
 
         // Arrange
         var texto = "Começo, meio e fim do texto procurado";
