c429dbba5496b9cc162340e384820fd2e21b1cf3
Jorgeluisreis <ctt.jorgeluis@gmail.com>
2024-01-14 23:30:59 -0300

    Desafio feito

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -1,27 +1,22 @@
 using TestesUnitarios.Desafio.Console.Services;
-
 namespace TestesUnitarios.Desafio.Tests;
-
 public class ValidacoesStringTests
 {
     private ValidacoesString _validacoes = new ValidacoesString();
-
     [Fact]
     public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
     {
         //TODO: Corrigir a variável "texto" e "resultadoEsperado" da seção Arrange
 
         // Arrange
-        var texto = "a";
-        var resultadoEsperado = 0;
+        var texto = "Matrix";
+        var resultadoEsperado = 6;
 
         // Act
         var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
-
         // Assert
         Assert.Equal(resultadoEsperado, resultado);
     }
-
     [Fact]
     public void DeveContemAPalavraQualquerNoTexto()
     {
@@ -31,11 +26,11 @@ public class ValidacoesStringTests
 
         //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
         // Act
-         _validacoes.ContemCaractere(texto, textoProcurado);
+        var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
         //TODO: Corrigir o Assert.True com base no retorno da chamada ao método
-        Assert.True(true);
+        Assert.True(resultado);
     }
 
     [Fact]
@@ -44,13 +39,12 @@ public class ValidacoesStringTests
         // Arrange
         var texto = "Esse é um texto qualquer";
         var textoProcurado = "teste";
-
         // Act
         var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
         //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
-        Assert.False(true);
+        Assert.False(resultado);
     }
 
     //TODO: Corrigir a anotação [Fact]
@@ -60,11 +54,10 @@ public class ValidacoesStringTests
 
         // Arrange
         var texto = "Começo, meio e fim do texto procurado";
-        var textoProcurado = "teste";
+        var textoProcurado = "procurado";
 
         // Act
         var resultado = _validacoes.TextoTerminaCom(texto, textoProcurado);
-
         // Assert
         Assert.True(resultado);
     }
